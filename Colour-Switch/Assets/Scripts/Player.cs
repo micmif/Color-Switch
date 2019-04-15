bac8d3ed52434020e1965b0093b0ebc87abfb9a6
Michela <micmif@gmail.com>
2019-04-15 16:36:29 +0200

    Base of Game
    Jumping player, rotating obstacle, random color for player.

---
+++ Colour-Switch/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string currentColor;
+    public float jumpforce = 10f;
+    public Rigidbody2D circle;
+    public SpriteRenderer _sr;
+    public Color blue;
+    public Color yellow;
+    public Color pink;
+    public Color purple;
+    // Update is called once per frame
+    void Start()
+    {
+        setRandomColor();
+    }
+    void Update()
+    {
+        // If you press space or left click (0), the player will jump up.
+        if(Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
+        {
+            circle.velocity = Vector2.up * jumpforce;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        Debug.Log(collision.tag);
+    }
+
+    void setRandomColor()
+    {
+        int rand = Random.Range(0, 3);
+
+        switch (rand)
+        {
+            case 0:
+                currentColor = "Blue";
+                _sr.color = blue;
+                break;           
+            case 1:
+                currentColor = "Yellow";
+                _sr.color = yellow; 
+                break;          
+            case 2:
+                currentColor = "Pink";
+                _sr.color = pink; 
+                break;
+            case 3:
+                currentColor = "Purple";
+                _sr.color = purple; 
+                break;
+        }
+    }
+}
